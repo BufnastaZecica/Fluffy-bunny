503ef18e4068bf2a78c0d22264b0669fe6d887e3
BufnastaZecica <regovicsanja@gmail.com>
2021-04-29 22:04:17 +0100

    Mob view and closing manu

--- js/index.js
+++ js/index.js
@@ -1,42 +1,45 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const l_bunny = entry.target.querySelector(".left-bunny");
+    const r_bunny = entry.target.querySelector(".right-bunny");
+    if (entry.isIntersecting) {
+      l_bunny.classList.add("animated-left");
+      r_bunny.classList.add("animated-right");
 
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          const l_bunny = entry.target.querySelector(".left-bunny");
-          const r_bunny = entry.target.querySelector(".right-bunny");
-          if (entry.isIntersecting) {
-            l_bunny.classList.add("animated-left");
-            r_bunny.classList.add("animated-right");
+      return; // if we added the class, exit the function
+    }
 
-            return; // if we added the class, exit the function
-          }
+    // We're not intersecting, so remove the class!
+    l_bunny.classList.remove("animated-left");
+    r_bunny.classList.remove("animated-right");
+  });
+});
+const tracker = document.querySelector(".animated-bunnies");
+observer.observe(tracker);
 
-          // We're not intersecting, so remove the class!
-          l_bunny.classList.remove("animated-left");
-          r_bunny.classList.remove("animated-right");
-        });
-      });
-      const tracker = document.querySelector(".animated-bunnies");
-      observer.observe(tracker);
+const trackerr = document.querySelector(".animated-bunniess");
+observer.observe(trackerr);
 
-      const trackerr = document.querySelector(".animated-bunniess");
-      observer.observe(trackerr);
-    
+function hideMenu() {
+  let menuOpen = document.querySelector("#checkbox").checked;
 
+  if ((menuOpen = true)) {
+    document.querySelector("#checkbox").checked = false;
+  }
+}
+window.addEventListener("scroll", hideMenu);
 
+//   (function () {
+//     emailjs.init("user_dXnw0wdAdI0EwpxJtJWnR");
+//     document
+//       .getElementById("contact-form")
+//       .addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         emailjs.sendForm("bufna", "bufnat", "contact-form").then(
+//           function (response) {
+//             document.getElementById("contact").style.display="none";
+//             document.getElementById("responce").style.display="flex";
 
-      
-    //   (function () {
-    //     emailjs.init("user_dXnw0wdAdI0EwpxJtJWnR");
-    //     document
-    //       .getElementById("contact-form")
-    //       .addEventListener("submit", function (event) {
-    //         event.preventDefault();
-    //         emailjs.sendForm("bufna", "bufnat", "contact-form").then(
-    //           function (response) {
-    //             document.getElementById("contact").style.display="none";
-    //             document.getElementById("responce").style.display="flex";
-
-    //       });
-    //   })
-    // })();
-    
+//       });
+//   })
+// })();
