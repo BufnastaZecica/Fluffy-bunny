eb8a4039d89ffa8c030bfc393c5dbff20b072120
BufnastaZecica <regovicsanja@gmail.com>
2021-07-05 10:18:31 +0100

    intersection observer

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const l_bunny = entry.target.querySelector(".left-bunny");
-    const r_bunny = entry.target.querySelector(".right-bunny");
-    if (entry.isIntersecting) {
-      l_bunny.classList.add("animated-left");
-      r_bunny.classList.add("animated-right");
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     const l_bunny = entry.target.querySelector(".left-bunny");
+//     const r_bunny = entry.target.querySelector(".right-bunny");
+//     if (entry.isIntersecting) {
+//       l_bunny.classList.add("animated-left");
+//       r_bunny.classList.add("animated-right");
 
-      return; // if we added the class, exit the function
-    }
+//       return; // if we added the class, exit the function
+//     }
 
-    // We're not intersecting, so remove the class!
-    l_bunny.classList.remove("animated-left");
-    r_bunny.classList.remove("animated-right");
-  });
-});
+//     // We're not intersecting, so remove the class!
+//     l_bunny.classList.remove("animated-left");
+//     r_bunny.classList.remove("animated-right");
+//   });
+// });
 // const tracker = document.querySelector(".animated-bunnies");
 // observer.observe(tracker);
 
-const trackerr = document.querySelector(".animated-bunniess");
-observer.observe(trackerr);
+
+function intersection(el, gs) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      gs.forEach(({tt, cls }) => {
+        const t = entry.target.querySelector(tt);
+        if (entry.isIntersecting) {
+          t.classList.add(cls);
+          return;
+        }
+        t.classList.remove(cls);
+      })
+    });
+  });
+  const tracker = document.querySelector(el);
+  observer.observe(tracker);
+}
+
+
+
+intersection(".animated-bunnies", [{tt: ".left-bunny", cls: "animated-left"}, {tt: ".right-bunny", cls: "animated-right"}]);
+intersection(".walk-bunny-container", [{tt: ".walking-bunny", cls:  "animated-bunny"}]);
 
 function hideMenu() {
   let menuOpen = document.querySelector("#checkbox").checked;
