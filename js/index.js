8a23d2e007e65a1faab0247c7fd5b657d969fcf0
BufnastaZecica <regovicsanja@gmail.com>
2021-09-10 22:56:22 +0100

    one page

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     const l_bunny = entry.target.querySelector(".left-bunny");
-//     const r_bunny = entry.target.querySelector(".right-bunny");
-//     if (entry.isIntersecting) {
-//       l_bunny.classList.add("animated-left");
-//       r_bunny.classList.add("animated-right");
 
-//       return; // if we added the class, exit the function
-//     }
-
-//     // We're not intersecting, so remove the class!
-//     l_bunny.classList.remove("animated-left");
-//     r_bunny.classList.remove("animated-right");
-//   });
-// });
-// const tracker = document.querySelector(".animated-bunnies");
-// observer.observe(tracker);
 
 
 function intersection(el, gs) {
@@ -40,26 +23,4 @@ function intersection(el, gs) {
 intersection(".animated-bunnies", [{tt: ".left-bunny", cls: "animated-left"}, {tt: ".right-bunny", cls: "animated-right"}]);
 intersection(".walk-bunny-container", [{tt: ".walking-bunny", cls:  "animated-bunny"}]);
 
-function hideMenu() {
-  let menuOpen = document.querySelector("#checkbox").checked;
 
-  if ((menuOpen = true)) {
-    document.querySelector("#checkbox").checked = false;
-  }
-}
-window.addEventListener("scroll", hideMenu);
-
-//   (function () {
-//     emailjs.init("user_dXnw0wdAdI0EwpxJtJWnR");
-//     document
-//       .getElementById("contact-form")
-//       .addEventListener("submit", function (event) {
-//         event.preventDefault();
-//         emailjs.sendForm("bufna", "bufnat", "contact-form").then(
-//           function (response) {
-//             document.getElementById("contact").style.display="none";
-//             document.getElementById("responce").style.display="flex";
-
-//       });
-//   })
-// })();
